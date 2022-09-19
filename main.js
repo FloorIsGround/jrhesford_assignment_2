b890028e142253c8060037f9ef8446a4c57e0e51
FloorIsGround <112002787+FloorIsGround@users.noreply.github.com>
2022-09-19 10:20:13 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,32 @@
+//Question 1
+let myNumber;
+
+//Question 2
+myNumber = 5;
+
+//Question 3
+let myOtherNumber = 5;
+
+//Question 4
+let myTotal;
+myTotal = myNumber + myOtherNumber;
+
+//Question 5
+let myName = "Jace";
+let myCombo = myNumber.toString() + myOtherNumber.toString(); //I use toString() to convert both number variables to strings so myCombo is the correct string type.
+
+//Question 6
+let myOtherCombo = myNumber + myOtherNumber;
+
+//question 7
+let myRemainder;
+myRemainder = myNumber % 5;
+
+//Question 8
+let i = 2;
+
+//Question 9
+let int = 4;
+
+//question 10
+let myBoolean = false;
